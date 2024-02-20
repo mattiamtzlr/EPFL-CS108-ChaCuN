1d18f134c45c6d5637cdad78a69efa8a9b427965
Mattia <mattia.metzler@gmail.com>
2024-02-20 10:20:18 +0100

    Actually use the stuff that is already there

--- src/ch/epfl/chacun/Rotation.java
+++ src/ch/epfl/chacun/Rotation.java
@@ -11,20 +11,12 @@ public enum Rotation {
     public static final List<Rotation> ALL = List.of(Rotation.values());
     public static final int COUNT = ALL.size();
 
-    private static Rotation fromOrdinal(int ordinal) {
-        for (Rotation rot : Rotation.values()) {
-            if (rot.ordinal() == ordinal) return rot;
-        }
-
-        return null;
-    }
-
     public Rotation add(Rotation that) {
-        return fromOrdinal((this.ordinal() + that.ordinal()) % COUNT);
+        return ALL.get((this.ordinal() + that.ordinal()) % COUNT);
     }
 
     public Rotation negated() {
-        return fromOrdinal((this.ordinal() + (COUNT / 2)) % COUNT);
+        return ALL.get((this.ordinal() + (COUNT / 2)) % COUNT);
     }
 
     public int QuarterTurnsCW() {
