389d39f78219f3468c89619f9cc49b91ae78826a
Mattia <mattia.metzler@gmail.com>
2024-02-23 12:12:23 +0100

    JavaDoc, delete Main, small changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CS-108 Project: ChaCuN
 
 ## Links
-- [Stage 1](https://cs108.epfl.ch/p/01_setup.html)
-- [Stage 2](https://cs108.epfl.ch/p/02_tiles.html)
+- [Step 1](https://cs108.epfl.ch/p/01_setup.html)
+- [Step 2](https://cs108.epfl.ch/p/02_tiles.html)
 
--- src/ch/epfl/chacun/Rotation.java
+++ src/ch/epfl/chacun/Rotation.java
@@ -17,18 +17,39 @@ public enum Rotation {
     public static final List<Rotation> ALL = List.of(Rotation.values());
     public static final int COUNT = ALL.size();
 
+    /**
+     * Adds another rotation to the current instance, e.g. adding RIGHT to HALF_TURN returns
+     * LEFT.
+     * @param that (Rotation) rotation to add
+     * @return (Rotation) sum of the rotations
+     */
     public Rotation add(Rotation that) {
         return ALL.get((this.ordinal() + that.ordinal()) % COUNT);
     }
 
+    /**
+     * Returns the opposite of the instance, meaning the rotation, which when added to the
+     * original one yields NONE.
+     * @return (Rotation) the opposite of the rotation
+     */
     public Rotation negated() {
         return ALL.get((COUNT - this.ordinal()) % COUNT);
     }
 
+    /**
+     * Returns the number of clock-wise quarter turns represented by the rotation, e.g. HALF_TURN
+     * is 2.
+     * @return (int) the number of clock-wise quarter turns
+     */
     public int quarterTurnsCW() {
         return this.ordinal();
     }
 
+    /**
+     * Returns the number of clock-wise degrees represented by the rotation, e.g. HALF_TURN is
+     * 180°.
+     * @return the number of degrees.
+     */
     public int degreesCW() {
         return this.ordinal() * 90;
     }
