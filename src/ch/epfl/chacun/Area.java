028b2a157cc7abd0fe790b5c3176bf9519ea9c94
Mattia <mattia.metzler@gmail.com>
2024-03-05 09:24:09 +0100

    Finished JavaDoc in Area.java

--- src/ch/epfl/chacun/Area.java
+++ src/ch/epfl/chacun/Area.java
@@ -3,17 +3,20 @@ package ch.epfl.chacun;
 import java.util.*;
 
 /**
- * TODO JavaDoc description
+ * Area record used to represent areas, a combination of multiple zones, usually spanning multiple
+ * tiles
  *
  * @author Mattia Metzler (372025)
  * @author Leoluca Bernardi (374107)
  */
 public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnections) {
     /**
-     * TODO JavaDoc
-     * @param zones
-     * @param occupants
-     * @param openConnections
+     * Constructs a new area with the given params, copies the zones and copies & sorts the
+     * occupants. Also checks that the openConnections param is >= 0
+     * @param zones (Set<Z>) the set of zones partitioning the area
+     * @param occupants (List<PlayerColor>) the list of players occupying the area, these are the
+     *                  colors, not the occupants themselves (!)
+     * @param openConnections (int) the number of open connections of the area
      */
     public Area {
         Preconditions.checkArgument(openConnections >= 0);
@@ -26,9 +29,9 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     // ================================================================== Static Methods
 
     /**
-     * TODO JavaDoc
-     * @param forest
-     * @return
+     * Method to check whether the given area of type forest has a zone with a menhir
+     * @param forest (Area<Zone.Forest>) the forest area to be checked
+     * @return true if there is at least one zone with a menhir
      */
     public static boolean hasMenhir(Area<Zone.Forest> forest) {
         for (Zone.Forest zone : forest.zones()) {
@@ -39,9 +42,9 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     }
 
     /**
-     * TODO Javadoc
-     * @param forest
-     * @return
+     * Method to count the total number of mushroom groups in the given forest area
+     * @param forest (Area<Zone.Forest>) the forest area of which the mushrooms are to be counted
+     * @return (int) the total number of mushrooms
      */
     public static int mushroomGroupCount(Area<Zone.Forest> forest) {
         int count = 0;
@@ -53,13 +56,14 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     }
 
     /**
-     * TODO JavaDoc
-     * @param meadow
-     * @param cancelledAnimals
-     * @return
+     * Method to get the set of animals living in an area of type meadow, animals which should not
+     * be considered (cancelledAnimals) are removed, e.g. dead deer
+     * @param meadow (Area<Zone.Meadow>) the meadow area to get the animals of
+     * @param cancelledAnimals (Set<Animal>) set of animals that should be removed
+     * @return (Set<Animal>) set of animals living in the area
      */
     public static Set<Animal> animals(Area<Zone.Meadow> meadow, Set<Animal> cancelledAnimals) {
-        Set<Animal> animals = new HashSet<Animal>();
+        Set<Animal> animals = new HashSet<>();
         for (Zone.Meadow zone : meadow.zones()) {
             animals.addAll(zone.animals());
         }
@@ -68,9 +72,9 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     }
 
     /**
-     * TODO JavaDoc
-     * @param river
-     * @return
+     * Method to count the number of fish in a river area
+     * @param river (Area<Zone.River>) the river area to count the fish in
+     * @return (int) number of fish in the river area
      */
     public static int riverFishCount(Area<Zone.River> river) {
         Set<Zone.Lake> knownLakes = new HashSet<>();
@@ -84,9 +88,9 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     }
 
     /**
-     * TODO JavaDoc
-     * @param riverSystem
-     * @return
+     * Method to count the number of fish in a river system
+     * @param riverSystem (Area<Zone.Water>) the river system to count the fish in
+     * @return (int) number of fish in the river system
      */
     public static int riverSystemFishCount(Area<Zone.Water> riverSystem) {
         // TODO: Doesn't this just do the same thing as riverFishCount() ?
@@ -98,9 +102,9 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     }
 
     /**
-     * TODO JavaDoc
-     * @param riverSystem
-     * @return
+     * Method to count the number of lakes in a river system
+     * @param riverSystem (Area<Zone.Water>) the river system to count the lakes in
+     * @return (int) number of lakes in the river system
      */
     public static int lakeCount(Area<Zone.Water> riverSystem) {
         int count = 0;
@@ -131,8 +135,9 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     }
 
     /**
-     * TODO JavaDoc, also this might be really inefficient.
-     * @return
+     * Returns the set of majority occupants of the current instance, meaning the set of player
+     * colors of which there are the most occupants in an area.
+     * @return (Set<PlayerColor>) the set of majority occupants
      */
     public Set<PlayerColor> majorityOccupants() {
         Set<PlayerColor> majorityOccupants = new HashSet<>();
@@ -152,9 +157,10 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     }
 
     /**
-     * TODO JavaDoc
-     * @param that
-     * @return
+     * Method that returns a new area which is the connection of the current instance with another
+     * area, or with itself
+     * @param that (Area<Z>) the area to connect to
+     * @return (Area<Z>) the connection of the two areas
      */
     public Area<Z> connectTo(Area<Z> that) {
         // maybe this needs to be removed later, depending on how the open connections are handled
@@ -163,7 +169,7 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
 
         // I think this is all that needs to be done, but TODO test extensively
         if (this.equals(that))
-            return new Area<Z>(
+            return new Area<>(
                     this.zones, this.occupants, this.openConnections - 2
             );
         else {
@@ -187,7 +193,7 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     public Area<Z> withInitialOccupant(PlayerColor occupant) {
         Preconditions.checkArgument(!this.isOccupied());
         this.occupants.add(occupant);
-        return new Area<Z>(this.zones, this.occupants, this.openConnections);
+        return new Area<>(this.zones, this.occupants, this.openConnections);
     }
 
     /**
@@ -198,12 +204,12 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
     public Area<Z> withoutOccupant(PlayerColor occupant) {
         Preconditions.checkArgument(this.occupants.contains(occupant));
         this.occupants.remove(occupant);
-        return new Area<Z>(this.zones, this.occupants, this.openConnections);
+        return new Area<>(this.zones, this.occupants, this.openConnections);
     }
 
     /**
      * Returns the same area as the current instance without any occupants.
-     * @return
+     * @return (Area<Z>) the same area without any occupants
      */
     public Area<Z> withoutOccupants() {
         return new Area<>(this.zones, Collections.emptyList(), this.openConnections);
