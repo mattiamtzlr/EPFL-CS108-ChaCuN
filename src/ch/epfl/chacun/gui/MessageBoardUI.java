6b4b45e9279436e38518ad7789735de1a6823d78
leolucabernardi <pixlpilz@outlook.de>
2024-04-25 08:42:58 +0200

    Stage 8: It cant be that simple

--- src/ch/epfl/chacun/gui/MessageBoardUI.java
+++ src/ch/epfl/chacun/gui/MessageBoardUI.java
@@ -4,9 +4,11 @@ package ch.epfl.chacun.gui;
 import ch.epfl.chacun.MessageBoard;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.util.List;
 import java.util.Set;
@@ -18,7 +20,6 @@ import java.util.Set;
  * @author Leoluca Bernardi (374107)
  */
 public class MessageBoardUI {
-
     private MessageBoardUI() {}
 
     /**
@@ -30,9 +31,18 @@ public class MessageBoardUI {
     public static Node create(ObservableValue<List<MessageBoard.Message>> observableMessages,
                               ObjectProperty<Set<Integer>> observableTileIds) {
         ScrollPane messageBoardRoot = new ScrollPane();
+        VBox scrollableMessages = new VBox();
+
         messageBoardRoot.setId("message-board");
         messageBoardRoot.setStyle("message-board.css");
-        VBox scrollBox = new VBox();
+        messageBoardRoot.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        messageBoardRoot.setContent(scrollableMessages);
+        scrollableMessages.setAlignment(Pos.TOP_LEFT);
+
+        for (MessageBoard.Message message : observableMessages.getValue()) {
+            Text text = new Text(message.text());
+            scrollableMessages.getChildren().add(text);
+        }
 
     }
 }
