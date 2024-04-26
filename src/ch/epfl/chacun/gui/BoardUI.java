779a0354be18021f456c217cf6eb3020926001e1
leolucabernardi <pixlpilz@outlook.de>
2024-04-26 17:33:39 +0200

    Stage 9: Inhale the future, exhale the past
    yeeeeeeeeeeee

--- src/ch/epfl/chacun/gui/BoardUI.java
+++ src/ch/epfl/chacun/gui/BoardUI.java
@@ -1,13 +1,20 @@
 package ch.epfl.chacun.gui;
 
 
-import ch.epfl.chacun.GameState;
-import ch.epfl.chacun.Occupant;
-import ch.epfl.chacun.Pos;
-import ch.epfl.chacun.Rotation;
+import ch.epfl.chacun.*;
+import static ch.epfl.chacun.Occupant.Kind.*;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
+import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
 
@@ -18,24 +25,101 @@ import java.util.function.Consumer;
  * @author Leoluca Bernardi (374107)
  */
 public final class BoardUI {
-    private BoardUI() {}
+    private BoardUI() {
+    }
 
-    public Node create(ObservableValue<GameState> observableGameState,
-                       ObservableValue<Rotation> observableRotation,
-                       ObservableValue<Set<Occupant>> observableOccupants,
-                       ObservableValue<Set<Integer>> observableHighlightedTiles,
-                       Consumer<Rotation> rotationHandler,
-                       Consumer<Pos> positionHandler,
-                       Consumer<Occupant> occupantHandler) {
+    public static Node create(
+            int boardSize,
+            ObservableValue<GameState> observableGameState,
+            ObservableValue<Rotation> observableRotation,
+            ObservableValue<Set<Occupant>> observableOccupants,
+            ObservableValue<Set<Integer>> observableHighlightedTiles,
+            Consumer<Rotation> rotationHandler,
+            Consumer<Pos> positionHandler,
+            Consumer<Occupant> occupantHandler) {
+
+        WritableImage emptyTileImage = new WritableImage(1, 1);
+        emptyTileImage
+                .getPixelWriter()
+                .setColor(0, 0, Color.gray(0.98));
         // Grid Pane
+        GridPane boardPane = new GridPane();
+        boardPane.setId("board-grid");
 
         // Two for loops to create each Group
+        for (int x = -boardSize; x <= boardSize; x++) {
+            for (int y = -boardSize; y <= boardSize; y++) {
+                //      ImageView for the Face of the tile
+                ImageView tileFace = new ImageView();
+                tileFace.setFitHeight(ImageLoader.NORMAL_TILE_FIT_SIZE);
+                tileFace.setFitWidth(ImageLoader.NORMAL_TILE_FIT_SIZE);
+                tileFace.imageProperty().setValue(emptyTileImage);
+                // Listener pointing to the correct tile on the board
+                int finalX = x;
+                int finalY = y;
+                Group boardSquare = new Group(tileFace);
 
-        //      Group
+                observableGameState.map(GameState::board)
+                    .addListener((_, _, newBoard) -> {
+                        PlacedTile tile = newBoard.tileAt(new Pos(finalX, finalY));
+                        if (Objects.nonNull(tile)) {
+                            System.out.println("yeah");
+                            tileFace.imageProperty()
+                                    .setValue(ImageLoader.normalForTileId(tile.id()));
 
-        //      ImageView for the Face of the tile
-        //      ImageView for the cancel markers
-        //      SVGPath for occupants
-        return null;
+                            tile.meadowZones().stream()
+                                .map(Zone.Meadow::animals)
+                                .flatMap(Collection::stream)
+                                .forEach(a -> {
+                                    ObservableValue<Boolean> isVisible =
+                                        new SimpleObjectProperty<>(newBoard.cancelledAnimals()
+                                                .contains(a));
+
+                                    ImageView cancellationMarker = new ImageView();
+                                    cancellationMarker.getStyleClass().add("marker");
+                                    cancellationMarker.setId(STR."marker_\{a.id()}");
+                                    cancellationMarker.visibleProperty().bind(isVisible);
+
+                                    boardSquare.getChildren().add(cancellationMarker);
+                                });
+
+                            tile.potentialOccupants()
+                                .forEach(
+                                    o -> {
+                                        Node svgPath = switch (o.kind()) {
+                                            case HUT ->
+                                                Icon.newFor(tile.placer(), HUT);
+                                            case PAWN ->
+                                                    Icon.newFor(tile.placer(), PAWN);
+                                        };
+
+                                        ObservableValue<Boolean> isVisible =
+                                                observableOccupants.map(s -> s.contains(o));
+                                        svgPath.visibleProperty().bind(isVisible);
+                                        svgPath.setId(o.kind().equals(HUT)
+                                                ? STR."hut_\{o.zoneId()}"
+                                                : STR."pawn_\{o.zoneId()}"
+                                        );
+                                         svgPath.rotateProperty()
+                                                 .set(tile.rotation().negated().degreesCW());
+                                         svgPath.setOnMouseClicked(_ -> occupantHandler.accept(o));
+
+                                        boardSquare.getChildren().add(svgPath);
+                                    }
+                                );
+
+                        }
+                    });
+
+                //      Group
+                boardPane.add(boardSquare, x + boardSize, y + boardSize);
+            }
+        }
+        // Scroll Pane
+        ScrollPane boardView = new ScrollPane(boardPane);
+        boardView.setId("board-scroll-pane");
+        boardView.getStylesheets().add("board.css");
+
+        return boardView;
     }
 }
---
+++ src/ch/epfl/chacun/gui/test/BoardUITest.java
@@ -0,0 +1,76 @@
+package ch.epfl.chacun.gui.test;
+
+import ch.epfl.chacun.*;
+import ch.epfl.chacun.gui.*;
+import ch.epfl.chacun.tile.Tiles;
+import javafx.application.Application;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static ch.epfl.chacun.PlayerColor.*;
+
+public final class BoardUITest extends Application {
+    public static void main(String[] args) {launch(args);}
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Map<PlayerColor, String> playerNames = Map.of(
+                PURPLE, "Leo",
+                YELLOW, "Mattia"
+        );
+
+        List<PlayerColor> playerColors = playerNames.keySet().stream()
+                .sorted()
+                .toList();
+
+        var tilesByKind = Tiles.TILES.stream()
+                .collect(Collectors.groupingBy(Tile::kind));
+
+        TileDecks tileDecks = new TileDecks(
+                tilesByKind.get(Tile.Kind.START),
+                tilesByKind.get(Tile.Kind.NORMAL),
+                tilesByKind.get(Tile.Kind.MENHIR)
+        );
+
+        TextMaker textMaker = new TextMakerFr(playerNames);
+
+        GameState gameState = GameState.initial(playerColors, tileDecks, textMaker);
+
+        var tileToPlaceRotationP =
+                new SimpleObjectProperty<>(Rotation.NONE);
+        var visibleOccupantsP =
+                new SimpleObjectProperty<>(Set.<Occupant>of());
+        var highlightedTilesP =
+                new SimpleObjectProperty<>(Set.<Integer>of());
+
+        var gameStateO = new SimpleObjectProperty<>(gameState);
+        var boardNode = BoardUI
+                .create(3,
+                        gameStateO,
+                        tileToPlaceRotationP,
+                        visibleOccupantsP,
+                        highlightedTilesP,
+                        r -> System.out.println("Rotate: " + r),
+                        t -> System.out.println("Place: " + t),
+                        o -> System.out.println("Select: " + o));
+
+        Occupant pawnR24 = new Occupant(Occupant.Kind.PAWN, 21);
+        PlacedTile t2WestOf56 = new PlacedTile(Tiles.TILES.get(2), YELLOW, Rotation.NONE, new Pos(-1, 0));
+        gameStateO.set(gameStateO.get().withStartingTilePlaced());
+
+        var rootNode = new BorderPane(boardNode);
+        primaryStage.setScene(new Scene(rootNode));
+
+        primaryStage.setTitle("ChaCuN test");
+        primaryStage.show();
+        gameStateO.set(gameStateO.get().withPlacedTile(t2WestOf56));
+        gameStateO.set(gameStateO.get().withNewOccupant(pawnR24));
+    }
+}
