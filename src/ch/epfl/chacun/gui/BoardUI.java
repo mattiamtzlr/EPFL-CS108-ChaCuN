bbf606c15072eb0cb61fb51877a74a7a26b593b3
leolucabernardi <pixlpilz@outlook.de>
2024-04-26 14:44:00 +0200

    Stage 9: Setup

---
+++ src/ch/epfl/chacun/gui/BoardUI.java
@@ -0,0 +1,13 @@
+package ch.epfl.chacun.gui;
+
+
+d
+/**
+ * TODO Description
+ *
+ * @author Mattia Metzler (372025)
+ * @author Leoluca Bernardi (374107)
+ */
+public final class BoardUI {
+    private BoardUI() {}
+}
