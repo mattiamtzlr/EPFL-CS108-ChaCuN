0eb712943f689e86f711dd7f2fd433cb166bd53c
leolucabernardi <pixlpilz@outlook.de>
2024-05-07 11:12:22 +0200

    Stage 10: Testing and improvement for ActionsUI
    ActionsUI does not have full functionality yet

--- src/ch/epfl/chacun/gui/ActionsUI.java
+++ src/ch/epfl/chacun/gui/ActionsUI.java
@@ -2,6 +2,8 @@ package ch.epfl.chacun.gui;
 
 import ch.epfl.chacun.Base32;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
@@ -11,6 +13,7 @@ import javafx.scene.text.Text;
 import java.util.List;
 import java.util.StringJoiner;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import static java.util.FormatProcessor.FMT;
 
@@ -33,9 +36,12 @@ public final class ActionsUI {
         actionHistory.textProperty().bind(
                 observableActions.map(oA -> {
                     StringJoiner text = new StringJoiner(", ");
-                    List<String> recentActions = oA.subList(oA.size() - 4, oA.size());
+                    int size = oA.size();
+                    List<String> recentActions = oA.subList(
+                            size - ((size > 3) ? 4 : size), size);
+                    
                     for (int i = 0; i < recentActions.size(); i++) {
-                        text.add(FMT."%2d\{i}:");
+                        text.add(FMT."%2d\{i}:\{recentActions.get(i)}");
                     }
                     return text.toString();
                 }));
@@ -46,8 +52,9 @@ public final class ActionsUI {
         actionInput.setTextFormatter(new TextFormatter<>(
                 change -> {
                     change.setText(change.getText().chars()
-                        .filter(c -> Base32.isValid(Character.toString(c)))
-                        .toString());
+                            .mapToObj(Character::toString)
+                            .filter(Base32::isValid)
+                            .collect(Collectors.joining()));
                     return change;
 
                 }));
---
+++ src/ch/epfl/chacun/gui/test/ActionsUITest.java
@@ -0,0 +1,44 @@
+package ch.epfl.chacun.gui.test;
+
+import ch.epfl.chacun.gui.ActionsUI;
+import ch.epfl.chacun.gui.ImageLoader;
+import javafx.application.Application;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO Description
+ *
+ * @author Mattia Metzler (372025)
+ * @author Leoluca Bernardi (374107)
+ */
+public class ActionsUITest extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        List<String> actions = new ArrayList<>();
+        actions.add("OOOOO");
+        ObservableValue<List<String>> observableActions = new SimpleObjectProperty<>(actions);
+        Node actionsUI = ActionsUI.create(
+                observableActions,
+                s -> System.out.println(STR."Action: \{s}")
+        );
+        BorderPane root = new BorderPane(actionsUI);
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("ActionsUI Test");
+        primaryStage.show();
+    }
+}
